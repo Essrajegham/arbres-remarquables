7c7ecd0e8e50996dd8565fbd470b323ecaf9b0d8
Essrajegham <votre-email@exemple.com>
2025-06-28 19:21:19 +0200

    Initial commit du projet complet (frontend + backend si applicable)

--- backend/middlewares/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = function authorizeRoles(...roles) {
-  return (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-      return res.status(403).json({ error: 'Accès refusé' });
-    }
-    next();
-  };
-};
